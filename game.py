f3b597cd1d47f5940dadec7d0df0bd5b928965b9
saeedpc <saeedmrpg@gmail.com>
2023-11-20 19:54:54 +0330

    fixed some bugs

--- game.py
+++ game.py
@@ -3,46 +3,59 @@ import random
 def get_input(prompt, type_func):
     while True:
         try:
-            user_input = type_func(input(prompt))
-            return user_input
+            inter = input(prompt)
+            if inter == "":
+                return inter
+            else:
+                user_input = type_func(inter)
+                return user_input
         except ValueError:
-            print("Invalid input. Please read the descripton try again ")
+            print("Invalid input. Please read the description and try again")
         except KeyboardInterrupt:
-            print("ok bye bye bye")
+            print("Ok bye bye bye")
             exit()
         except Exception as e:
-            print("unknown keyboard", e)
+            print("Unknown error:", e)
+
 def main():
-    print('Welcome to my game. Please enter two numbers which I will guess.',"\n\n guide: \n 1)give me two numbers number a bigger than number b\n\n 2)if you don't want to give me numbers skeep that i will use the a = 0 and b = 100 and count = 4\n\n 3)ctrl+c => exit()\n\n")
+    print('Welcome to my game. Please enter two numbers which I will guess.\n\nGuide:\n1) Give me two numbers, number a bigger than number b.\n2) If you don\'t want to give me numbers, I will use a = 0, b = 100, and count = 4.\n3) Ctrl+C => exit()\n')
     a = get_input('a = add number: ', int)
     b = get_input('b = add number bigger than a: ', int)
+    if a == "" and b == "":
+        a = 0 
+        b = 100
     if a + 1 >= b:
         print("a must be smaller than b. Please try again.")
         return
-    counter = get_input('counter = How many times I can guess: ', int) or 4
-    ready = get_input('If you are ready, please enter y. If not, enter n: ',str)
+    counter = get_input('counter = How many times I can guess: ', int)
+    ready = get_input('If you are ready, please enter y. If not, enter n: ', str)
     if ready != 'y':
         print('When you are ready, come back and play.')
         return
-    shomaresh = counter + 1
-    while ready == 'y':
-        hads = random.randint(a = 0, b = 100)
-        shomaresh -= 1
-        if shomaresh == 0:
-            print('I tried', counter, 'times and I lose!!!')
-            break
-        print('My guess:', hads)
-        print('I have', shomaresh - 1, 'chances left.')
-        rahnema = get_input('Was my guess correct? (b => big, s => small, t => true): ',str)
-        if rahnema == 'b':
-            b = hads - 1
-        elif rahnema == 's':
-            a = hads + 1
-        elif rahnema == 't':
-            print('I tried', shomaresh + 1, 'times and I win. Your number is:', hads)
-            break
-        else:
-            break
-
+    if counter == "":
+        counter = 4
+    shomaresh_count = counter + 1
+    try:
+        while ready == 'y':
+            hads = random.randint(a, b)
+            shomaresh_count -= 1
+            if shomaresh_count == 0:
+                print('I tried', counter, 'times and I lose!!!')
+                break
+            print('My guess:', hads)
+            print('I have', shomaresh_count - 1, 'chances left.')
+            rahnema = get_input('Was my guess correct? (b => big, s => small, t => true): ', str)
+            if rahnema == 'b':
+                b = hads - 1
+            elif rahnema == 's':
+                a = hads + 1
+            elif rahnema == 't':
+                print('I tried', shomaresh_count + 1, 'times and I win. Your number is:', hads)
+                break
+            else:
+                break
+    except Exception:
+        print('hey something wrong happened i think i guess true and you lie to me')
+        exit()
 if __name__ == '__main__':
     main()
